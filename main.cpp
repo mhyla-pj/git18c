22ea779c601a51afe57e5a4badee9ad61cb96533
Michal Hyla <mhyla@pjwstk.edu.pl>
2023-12-18 06:45:13 -0500

    added division

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main(){
 	int a;
 	cin>>a;
-	cout<< a+10;
+	cout<< a+10<<endl;
 	cout<<"odejmowanie";
 	cout<<a-10<<endl;
 	cout<<"mnozenie: ";
-	cout<<a*10;
+	cout<<a*10<<endl;
+	cout<<"dzielenie: ";
+	cout<<a/10<<endl;
 	return 0;
 }
